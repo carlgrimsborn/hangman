788310a73560a5e66900cab2ee2c845c7ddbf549
Carl Grimsborn <carlgrimsborn@gmail.com>
2025-01-30 16:47:18 +0100

    image functionality

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from tkinter import messagebox
 
 root = Tk()
 root.title("Hangman")
@@ -15,20 +16,39 @@ number_of_guesses = 0
 user_input = StringVar()
 
 
+def game_over():
+    global number_of_guesses
+    global hangman_img
+
+    messagebox.showinfo("Game Over", "Your game is over, try again")
+    number_of_guesses = 0
+    hangman_img = ImageTk.PhotoImage(Image.open("images/hangman1.png"))
+    imgLabel = Label(image=hangman_img)
+    imgLabel.grid(row=0, column=0, columnspan=5)
+
+
 def submit():
     global number_of_guesses
     global hangman_img
+
     number_of_guesses += 1
 
     hangman_img = ImageTk.PhotoImage(
-        Image.open("images/hangman" + str(number_of_guesses) + ".png")
+        Image.open("images/hangman" + str(number_of_guesses + 1) + ".png")
     )
     imgLabel = Label(image=hangman_img)
     imgLabel.grid(row=0, column=0, columnspan=5)
 
     value = user_input.get()
-    print(value, number_of_guesses, "images/hangman" + str(number_of_guesses) + ".png")
+    print(
+        value, number_of_guesses, "images/hangman" + str(number_of_guesses + 1) + ".png"
+    )
     user_input.set("")
+
+    if number_of_guesses >= 8:
+        root.update_idletasks()
+        game_over()
+        return
 
 
 label = Label(root, text="Enter your guess")
